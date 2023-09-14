642635d3e04399700a47591aa0d41cd6c39e63f1
leminhtuan <leminhtuanlmt2k1@gmail.com>
2023-09-14 19:29:22 +0700

    depploy

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 import classNames from 'classnames/bind';
 import styles from './Calendar.module.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
 
